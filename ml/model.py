4d25f8705b88f9fca4421790be682153896f185d
Dhudd22 <dhudd22@wgu.edu>
2025-05-11 19:33:01 -0500

    model.py implementation

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,8 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +22,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    rand_f = RandomForestClassifier()
+    rand_f.fit(X_train, y_train)
+    
+    return rand_f
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +65,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    predictions = model.predict(X)
+    
+    return predictions
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +80,15 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    pickle.dump(model, path)
+    
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    model_file = pickle.load(path)
+    
+    return model_file
 
 
 def performance_on_categorical_slice(
@@ -122,7 +132,13 @@ def performance_on_categorical_slice(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        data[data[column_name] == slice_value],
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
