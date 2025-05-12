9da9c8a8b415f4a0308ed688b760ab440b2b4bad
Dhudd22 <dhudd22@wgu.edu>
2025-05-11 21:49:43 -0500

    2nd try

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,17 @@
 import pytest
 from ml.model import compute_model_metrics, train_model, inference
-# TODO: add necessary import
 
-# TODO: implement the first test. Change the function name and input as needed
+
+project_path = os.getcwd()
+data_path = os.path.join(project_path, "data", "census.csv")
+print(data_path)
+data = pd.read_csv(data_path)
+
 def metrics_test():
     """
     Test to ensure the model metrics are populating as expected
     """
-    # Your code here
+  
     y_true, y_preds = [1, 1, 0], [0, 1, 1]
     precision, recall, fbeta = compute_model_metrics(y_true, y_preds)
     assert precision is not None
@@ -22,10 +26,7 @@ def load_base_data_test():
     test to ensure data was loaded correctly
     """
     # Your code here
-    project_path = os.getcwd()
-    data_path = os.path.join(project_path, "data", "census.csv")
-    print(data_path)
-    data = pd.read_csv(data_path)
+   
 
     assert data.shape == (32562, 15)
     
